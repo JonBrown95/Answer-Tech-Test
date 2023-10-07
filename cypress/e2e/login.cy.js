691af7bf9315a05639fd586ff50f82bec298138a
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-10-07 19:25:47 +0100

    Added test to check for invalid username

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,22 @@
-describe('Login Test', () => {
-  it('should login with valid credentials', () => {
-    cy.visit('https://www.saucedemo.com')
-    cy.get("#user-name").type("standard_user").should('have.value', 'standard_user')
-    cy.get("#password").type("secret_sauce").should('have.value', 'secret_sauce')
+describe("Login Test", () => {
+  it("should display an error message for invalid username", () => {
+    cy.visit("https://www.saucedemo.com");
+    cy.get("#user-name").type("invalid_user");
+    cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
-  })
-})
+    cy.contains(
+      "Epic sadface: Username and password do not match any user in this service"
+    ).should("be.visible");
+  });
+
+  it("should login with valid credentials", () => {
+    cy.visit("https://www.saucedemo.com");
+    cy.get("#user-name")
+      .type("standard_user")
+      .should("have.value", "standard_user");
+    cy.get("#password")
+      .type("secret_sauce")
+      .should("have.value", "secret_sauce");
+    cy.get("#login-button").click();
+  });
+});
