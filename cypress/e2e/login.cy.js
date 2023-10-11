51aaf05396d1f3432137e695a2bd6c205cf7bfe2
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-10-11 17:24:04 +0100

    Adjusted code to include test id selectors

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,42 +4,38 @@ describe("Login Test", () => {
   });
 
   it("should display an error message for invalid username", () => {
-    cy.get("#user-name").type("invalid_user");
-    cy.get("#password").type("secret_sauce");
-    cy.get("#login-button").click();
-    cy.contains(
-      "Epic sadface: Username and password do not match any user in this service"
-    ).should("be.visible");
+    cy.get('[data-test="username"]').type("invalid_user");
+    cy.get('[data-test="password"]').type("secret_sauce");
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]').should("be.visible");
   });
 
   it("should display an error message if invalid password entered", () => {
-    cy.get("#user-name").type("standard_user");
-    cy.get("#password").type("invalid_password");
-    cy.get("#login-button").click();
-    cy.contains(
-      "Epic sadface: Username and password do not match any user in this service"
-    ).should("be.visible");
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="password"]').type("invalid_password");
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]').should("be.visible");
   });
 
   it("should display an error message if no username is entered", () => {
-    cy.get("#login-button").click();
-    cy.contains("Epic sadface: Username is required").should("be.visible");
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]').should("be.visible");
   });
 
   it("should display an error message if no password is entered", () => {
-    cy.get("#user-name").type("standard_user");
-    cy.get("#login-button").click();
-    cy.contains("Epic sadface: Password is required").should("be.visible");
+    cy.get('[data-test="username"]').type("standard_user");
+    cy.get('[data-test="login-button"]').click();
+    cy.get('[data-test="error"]').should("be.visible");
   });
 
   it("should login with standard user credentials", () => {
-    cy.get("#user-name")
+    cy.get('[data-test="username"]')
       .type("standard_user")
       .should("have.value", "standard_user");
-    cy.get("#password")
+    cy.get('[data-test="password"]')
       .type("secret_sauce")
       .should("have.value", "secret_sauce");
-    cy.get("#login-button").click();
+    cy.get('[data-test="login-button"]').click();
     cy.url().should("include", "inventory.html");
   });
 });
